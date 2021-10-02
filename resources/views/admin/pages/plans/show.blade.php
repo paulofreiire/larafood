64ab3c73315804fb9f07bb11397748ed19150a8c
Paulo Freire <paulo.freiire@hotmail.com>
2021-10-02 02:19:26 -0300

    done plan show

---
+++ resources/views/admin/pages/plans/show.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('title', "Detalhes do Plano {$plan->name}")
+
+@section('content_header')
+    <h1>Detalhes do Plano <b>{{ $plan->name }}</b></h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+<ul>
+    <li>
+        <strong>Nome:</strong> {{$plan->name}}
+    </li>
+    <li>
+        <strong>Url:</strong> {{$plan->url}}
+    </li>
+    <li>
+        <strong>Preço:</strong> {{number_format($plan->price, 2, ',', '.')}}
+    </li>
+    <li>
+        <strong>Descrição:</strong> {{$plan->description}}
+    </li>
+</ul>
+        </div>
+    </div>
+@endsection
